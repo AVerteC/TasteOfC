182a940ffd813b0ca98eb4512880ac3eba8b5408
Alazorth <alan.r.chen@gmail.com>
2018-07-10 15:57:49 -0700

    finished up to ex02

---
+++ d02/ex02/ft_print_numbers.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+int ft_putchar(char c){
+    write(1, &c,1);
+    return 0;
+}
+
+void ft_print_numbers(void){
+    char c = '0';
+    while(c <= '9'){
+        ft_putchar(c);
+        c++;
+    }
+    ft_putchar('\n');
+}
+
+int main(void){
+    ft_print_numbers();
+}
