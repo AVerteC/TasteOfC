8a02a3e789b1892e7f330289c0492aa913335c09
Alazorth <alan.r.chen@gmail.com>
2018-07-12 15:35:14 -0700

    still working om ft_putnbr

--- d02/ex06/ft_putnbr.c
+++ d02/ex06/ft_putnbr.c
@@ -13,22 +13,23 @@ void ft_putnbr(int nb)
         ft_putchar('0');
     }
     
+    /*
     if (nb < 0)
     {
         ft_putchar('-');
         nb = -nb;
     }
+    */
 
-    if (nb < 10 && nb > 0)
+    if (nb > 0 && nb <=9)
     {
         ft_putchar(nb);
     }
-    else 
-    {
-    ft_putchar(nb % 10);
-    nb = nb/10;
-    ft_putchar(nb);  
-    }
+    
+    if(nb/10)
+        ft_putchar(nb%10 + '0')
+
+
 
 
 }
@@ -38,7 +39,7 @@ int main(void)
 {
     ft_putnbr(0);
     ft_putchar('\n');
-    ft_putnbr(2);
+    ft_putnbr(9);
     ft_putchar('\n');
     ft_putnbr(-100);
     ft_putchar('\n');
